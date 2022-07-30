5de6a6649aee8bc26193f49a05ebb875292b8b0b
Atsighi Bright <brytebee@gmail.com>
2022-07-30 16:13:10 +0100

    :white_check_mark: Update file structure
    Render all components in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import { Navbar } from "./components";
+import { About, Footer, Header, Skills, Testimonials, Work } from "./container";
 const App = () => {
   return (
     <div className="app">
       <Navbar />
+      <Header />
+      <About />
+      <Skills />
+      <Work />
+      <Testimonials />
+      <Footer />
     </div>
   );
 };
