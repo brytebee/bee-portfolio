d1050ac568edc298e9ae1f64b903ff675a21e786
Atsighi Bright <brytebee@gmail.com>
2022-07-31 17:49:01 +0100

    :heavy_plus_sign: Add a settings file
    Added .prettierrc and update App.js with Adrian's style snippets

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { Navbar } from "./components";
 import { About, Footer, Header, Skills, Testimonials, Work } from "./container";
+import "./App.scss";
+
 const App = () => {
   return (
     <div className="app">
