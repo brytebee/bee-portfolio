ee57ad8ce5d4b3377c4b8a539dce3d21c8d2c826
Atsighi Bright <brytebee@gmail.com>
2022-07-31 18:24:41 +0100

    :white_check_mark: Update Navbar.jsx
    Rendered nav links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React from 'react';
 
-import "./Navbar.scss";
+import { images } from '../../constants';
+import './Navbar.scss';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav>
+      <div>
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul>
+        {['home', 'about', 'skills', 'work', 'contact'].map((item) => (
+          <li key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
