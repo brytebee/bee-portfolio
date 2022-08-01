aa31ab46eae899c11d6270375db967d8751a1acd
Atsighi Bright <brytebee@gmail.com>
2022-08-01 10:48:43 +0100

    :white_check_mark: Update Navbar.jsx
    Added more circles to make the header interesting

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -4,9 +4,20 @@ import { motion } from 'framer-motion';
 import { images } from '../../constants';
 import './Header.scss';
 
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut',
+    },
+  },
+};
+
 const Header = () => {
   return (
-    <div className="app__header app__flex">
+    <div id="home" className="app__header app__flex">
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.5 }}
@@ -20,7 +31,38 @@ const Header = () => {
               <h1 className="head-text">Miceal</h1>
             </div>
           </div>
+
+          <div className="tag-cmp app__flex">
+            <p className="p-text">Web developer</p>
+            <p className="p-text">Freelancer</p>
+          </div>
         </div>
+      </motion.div>
+
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__header-img"
+      >
+        <img src={images.profile} alt="profile" />
+        <motion.img
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 1, ease: 'easeInOut' }}
+          src={images.circle}
+          alt="profile_circle"
+          className="overlay_circle"
+        />
+        <motion.div
+          variants={scaleVariants}
+          whileInView={scaleVariants.whileInView}
+          className="app__header-circles"
+        >
+          {[images.flutter, images.redux, images.sass].map((circle) => (
+            <div className="circle-cmp app__flex" key={circle}>
+              <img src={circle} alt="circle" />
+            </div>
+          ))}
+        </motion.div>
       </motion.div>
     </div>
   );
