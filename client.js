082832a632a8ead896b7582bd5fa8621e32e8d27
Atsighi Bright <brytebee@gmail.com>
2022-08-03 12:27:08 +0100

    :white_check_mark: Update client.js
    Added configuration to connect sanity to ReactJS

--- client.js
+++ client.js
@@ -1,1 +1,10 @@
 import sanityClient from '@sanity/client';
+import imageUrlBuilder from '@sanity/image-url';
+
+export const client = sanityClient({
+  projectId: process.env.REACT_APP_PORTFOLIO_BACKEND_SANITY_PROJECT_ID,
+  dataset: 'production',
+  apiVersion: '2022-08-03',
+  useCdn: true,
+  token: process.env.REACT_APP_PORTFOLIO_BACKEND_SANITY_TOKEN,
+});
