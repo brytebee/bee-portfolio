a30d11e9580a094cffbaeb18e71ea1ebe5d74296
Atsighi Bright <brytebee@gmail.com>
2022-08-04 08:50:54 +0100

    :white_check_mark: Update Work.jsx
    Wrapped Work in wrapper

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { AiFillEye, AiFillGithub } from 'react-icons/ai';
 import { motion } from 'framer-motion';
 
-// import { AppWrap } from '../../wrapper';
+import { AppWrap } from '../../wrapper';
 import { urlFor, client } from '../../client';
 import './Work.scss';
 
@@ -117,4 +117,4 @@ const Work = () => {
   );
 };
 
-export default Work;
+export default AppWrap(Work, 'works');
