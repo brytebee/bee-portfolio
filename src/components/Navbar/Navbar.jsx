a97ddef0e6f2755c0d044d4da9c8eb41930920d5
Atsighi Bright <brytebee@gmail.com>
2022-08-22 18:12:16 +0100

    :bug: Update Navbar.jsx
    Fixed hamburger bug not opening navbar onclick

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -29,8 +29,8 @@ const Navbar = () => {
 
         {toggle && (
           <motion.div
-            whileInView={{ x: [300, 0] }}
-            transition={{ duration: 0.85, ease: 'easeOut' }}
+            whileInView={{ x: [250, 0] }}
+            transition={{ duration: 0.4, ease: 'easeOut' }}
           >
             <HiX onClick={() => setToggle(false)} />
             <ul>
