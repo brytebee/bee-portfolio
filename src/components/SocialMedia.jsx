cf41809162a92b6c6b5ebc34571c3551baad214f
Atsighi Bright <brytebee@gmail.com>
2022-11-05 08:30:51 +0100

    :art: Update SocialMedia.jsx
    Created a socials object to hold all social media handles

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -2,11 +2,29 @@ import React from 'react';
 import { BsTwitter, BsInstagram } from 'react-icons/bs';
 import { FaFacebookF } from 'react-icons/fa';
 
+const socials = {
+  twitter: 'https://twitter.com/brytebee',
+  linkedIn: 'https://linkedin.com/in/brytebee',
+  github: 'https://github.com/brytebee',
+  instagram: 'https://www.instagram.com/bryte_bee',
+  facebook: 'https://web.facebook.com/brytebee',
+};
+
 const SocialMedia = () => {
   return (
     <div className="app__social">
       <div>
-        <a href="http://" target="_blank" rel="noopener noreferrer">
+        <a href={socials.twitter} target="_blank" rel="noopener noreferrer">
+          <BsTwitter />
+        </a>
+      </div>
+      <div>
+        <a href={socials.twitter} target="_blank" rel="noopener noreferrer">
+          <BsTwitter />
+        </a>
+      </div>
+      <div>
+        <a href={socials.twitter} target="_blank" rel="noopener noreferrer">
           <BsTwitter />
         </a>
       </div>
